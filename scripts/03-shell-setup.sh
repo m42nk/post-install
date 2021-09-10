d169fd56760194623ede194bbe5f7963cf430086
Syakhisk <mail.syakhisk@gmail.com>
2021-09-11 02:18:55 +0700

    oh-my-zsh unattended install

--- scripts/03-shell-setup.sh
+++ scripts/03-shell-setup.sh
@@ -9,7 +9,7 @@ $INSTALL zsh
 chsh -s "`which zsh`" "`whoami`"
 
 # Install oh-my-zsh
-ZSH="$HOME/.config/oh-my-zsh" sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+ZSH="$HOME/.config/oh-my-zsh" sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
 
 # Install starship prompt
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
