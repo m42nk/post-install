74e804152af623dd34b4278dda2f1d6b4727ea93
Syakhisk <mail.syakhisk@gmail.com>
2021-09-12 00:23:26 +0700

    fix fnm

--- scripts/04-node.sh
+++ scripts/04-node.sh
@@ -1,7 +1,12 @@
 #!/bin/env bash
 
 # Install fnm (fast node manager)
-mkdir -p $HOME/.local/share/fnm
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.local/share/fnm" --skip-shell
+FNMDIR="$HOME/.local/share/fnm"
+FNM="$HOME/.local/share/fnm/fnm"
+mkdir -p "$FNMDIR"
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$FNMDIR" --skip-shell
 
-fnm completions --shell `basename $SHELL`
+$FNM completions --shell `basename $SHELL`
+eval "$($FNM env)"
+export PATH="$FNMDIR:$PATH"
+$FNM install --lts
