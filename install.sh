8853931738aab91f7e9a2ab43ff95d577af9806f
Syakhisk <mail.syakhisk@gmail.com>
2021-09-12 00:26:39 +0700

    uncomment

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
 export ROOTDIR=`pwd`
-# ./scripts/00-get-pkg-mgr.sh
-# ./scripts/01-setup.sh
-# ./scripts/02-enter-sudo-pw-once-only.sh
-# ./scripts/03-shell-setup.sh
+./scripts/00-get-pkg-mgr.sh
+./scripts/01-setup.sh
+./scripts/02-enter-sudo-pw-once-only.sh
+./scripts/03-shell-setup.sh
 ./scripts/04-node.sh
-# ./scripts/05-python.sh
-# ./scripts/06-vim.sh
-# ./scripts/07-other-packages.sh
+./scripts/05-python.sh
+./scripts/06-vim.sh
+./scripts/07-other-packages.sh
